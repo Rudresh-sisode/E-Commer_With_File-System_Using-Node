8d074877772999d0348df35567877b436fd17f33
Sohame Rajput <rudranrajput@gmail.com>
2020-12-08 18:41:17 +0530

    all file change with sql sequence npm package

--- E_Commerse_With_SQL/util/database.js
+++ E_Commerse_With_SQL/util/database.js
@@ -1,6 +1,6 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize'); 
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node-complete', 'root', 'rudsiso14140', {
   dialect: 'mysql',
   host: 'localhost'
 });
